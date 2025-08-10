4493636fda4d0c573318b02c88c708e8f1ccf0a0
Tadeas Burda <tadeas.burda@sensoneo.com>
2025-08-10 12:32:45 +0200

    Add dependency configuration and package references
    Introduce new package references for Microsoft.Extensions and Windows SDKs.
    Create DependencyConfiguration class for service management,
    including methods for configuring services and retrieving them.

---
+++ WinUISharedLibrary/DependencyConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
+
+namespace WinUISharedLibrary;
+
+/// <summary>
+/// Provides methods for configuring and retrieving services.
+/// </summary>
+public static class DependencyConfiguration
+{
+    /// <summary>
+    /// Gets or sets the main window of the application.
+    /// </summary>
+    public static Window? MainWindow { get; set; }
+
+    /// <summary>
+    /// Gets or sets the service provider.
+    /// </summary>
+    public static ServiceProvider? Services { get; set; }
+
+    /// <summary>
+    /// Configures the specified service collection.
+    /// </summary>
+    /// <param name="services">The service collection to configure.</param>
+    public static void Configure(ServiceCollection services)
+    {
+        services.Configure();
+    }
+
+    /// <summary>
+    /// Gets the specified service.
+    /// </summary>
+    /// <typeparam name="T">The type of service to get.</typeparam>
+    /// <returns>The requested service.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if the service provider is not configured.</exception>
+    public static T GetService<T>()
+        where T : class
+    {
+        if (Services == null)
+        {
+            throw new InvalidOperationException("Services is null");
+        }
+
+        return Services.GetRequiredService<T>();
+    }
+}
